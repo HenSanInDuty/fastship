bce8591e67aa58d6862128490d37ce0f16764067
= <=>
2022-11-20 05:35:31 +0700

    Make up home

--- fastship/lib/home/home.dart
+++ fastship/lib/home/home.dart
@@ -12,30 +12,75 @@ class _HomeState extends State<Home> {
   final storage = const FlutterSecureStorage();
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
         appBar: AppBar(
-          title: Text("Home"),
+          title: const Text("Home"),
           automaticallyImplyLeading: false,
         ),
-        body: Column(
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/all-shipping");
-                },
-                child: const Text("All shipping")),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/map-all-package");
-                },
-                child: const Text("Map tracking all shipping")),
-            ElevatedButton(
-                onPressed: () async {
-                  await storage.delete(key: 'api_key');
-                  Navigator.pushNamed(context, "/login");
-                },
-                child: const Text("Logout"))
-          ],
+        body: SizedBox(
+          height: height,
+          width: width,
+          child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              TextButton.icon(
+                  style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(24),
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          side: BorderSide(color: Colors.blue))),
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/all-shipping");
+                  },
+                  icon: const Icon(Icons.local_shipping),
+                  label: const Text("All shipping")),
+              const SizedBox(
+                height: 20,
+              ),
+              TextButton.icon(
+                  style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(24),
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          side: BorderSide(color: Colors.amber))),
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/map-all-package");
+                  },
+                  icon: const Icon(
+                    Icons.public,
+                    color: Colors.amber,
+                  ),
+                  label: const Text(
+                    "All Package On Map",
+                    style: TextStyle(color: Colors.amber),
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+              TextButton.icon(
+                  style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(24),
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          side: BorderSide(color: Colors.grey))),
+                  onPressed: () async {
+                    await storage.delete(key: 'api_key');
+                    setState(() {
+                      Navigator.pushNamed(context, "/login");
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.logout,
+                    color: Colors.grey,
+                  ),
+                  label: const Text(
+                    "Log out",
+                    style: TextStyle(color: Colors.grey),
+                  )),
+            ]),
+          ),
         ));
   }
 }
